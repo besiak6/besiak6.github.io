5dc517199bc01734ae5757d1be9cf30638318121
besiak6 <besi4k6@gmail.com>
2025-12-08 14:18:45 +0100

    Update zw.js

--- scripts/baddonz/zw.js
+++ scripts/baddonz/zw.js
@@ -1,26 +1,28 @@
 const isOtherInBattleRange = (other) => {
-    const { x: hx, y: hy } = Engine.hero.d;
-    const { x, y } = other.d;
-    return Math.max(Math.abs(x - hx), Math.abs(y - hy)) <= 20;
+  const { x: hx, y: hy } = Engine.hero.d;
+  const { x, y } = other.d;
+  return Math.max(Math.abs(x - hx), Math.abs(y - hy)) <= 20;
 };
 const updatePartyMembers = () => {
-    if (!Engine.party) return;
-    const others = Engine.others.check();
-    const members = Engine.party.getMembers();
-    const { id: hid } = Engine.hero.d;
-    for (const id of Object.keys(members)) {
-        if (id == hid) continue;
-        const $nickname = members[id].$.find('.nickname');
-        const inRange = others[id] && isOtherInBattleRange(others[id]);
-        $nickname.css('color', inRange ? '' : 'red');
-    }
+  if (!Engine.party || !Engine.party.getMembers()) return;
+  const others = Engine.others.check();
+  const { id: hid } = Engine.hero.d;
+  Engine.party.get$Wnd()[0].querySelector(".amount").innerText = `(${
+    Engine.party.getMembers().size
+  })`;
+  Engine.party.getMembers().forEach((c, v) => {
+    if (v == hid) return;
+    const inRange = others[v] && isOtherInBattleRange(others[v]);
+    c.el.querySelector(".nickname-text").style.color = inRange ? "" : "red";
+  });
 };
-const intercept = (obj, key, cb, _ = obj[key]) => obj[key] = (...args) => {
+const intercept = (obj, key, cb, _ = obj[key]) =>
+  (obj[key] = (...args) => {
     const result = _.apply(obj, args);
     return cb(...args) ?? result;
-};
-intercept(Engine.communication, 'parseJSON', (data) => {
-    if (data.h || data.party || data.other) {
-        updatePartyMembers();
-    }
+  });
+intercept(Engine.communication, "parseJSON", (data) => {
+  if (data.h || data.party || data.other) {
+    updatePartyMembers();
+  }
 });
