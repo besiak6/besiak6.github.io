319c38e7554fb6eb844eac83e3fb50d52e5dee8d
besiak6 <besi4k6@gmail.com>
2026-06-26 13:47:59 +0200

    Update beta.user.js

--- scripts/beta.user.js
+++ scripts/beta.user.js
@@ -6,18 +6,15 @@
 // @match         https://*.margonem.pl/*
 // @grant         none
 // @icon          https://i.imgur.com/OAtRFEw.png
-// @downloadURL   https://besiak6.github.io/scripts/beta.user.js
-// @updateURL     https://besiak6.github.io/scripts/beta.user.js
+// @downloadURL   https://besiak6.github.io/scripts/baddonz.user.js
+// @updateURL     https://besiak6.github.io/scripts/baddonz.user.js
 // ==/UserScript==
 (function () {
     const v = localStorage.getItem('bdz_ver') || '0';
+    window.CSS_URL = `https://besiak6.github.io/styles/beta.css?v=${v}`;
     const s = document.createElement('script');
     s.src = `https://besiak6.github.io/scripts/beta.js?v=${v}`;
     document.head.appendChild(s);
-    const l = document.createElement('link');
-    l.rel = 'stylesheet';
-    l.href = `https://besiak6.github.io/styles/beta.css?v=${v}`;
-    document.head.appendChild(l);
     fetch(`https://besiak6.github.io/scripts/version.json?t=${Date.now()}`)
         .then(r => r.json())
         .then(d => { if (d?.version !== v) localStorage.setItem('bdz_ver', d.version); })
